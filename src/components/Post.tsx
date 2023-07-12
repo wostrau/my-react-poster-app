6cd5067b8feb45197444f617ef9cd879bf86760d
wostrau <a.ostrovtsov@gmail.com>
2023-07-12 18:40:13 +0200

    #508 - DONE

---
+++ src/components/Post.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Post: React.FC = () => {
+  return (
+    <div>
+      <p>Aliaksandr</p>
+      <p>ReactJS is normal technology!</p>
+    </div>
+  );
+};
+
+export default Post;
